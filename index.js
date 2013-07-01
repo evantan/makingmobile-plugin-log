f75d0aeed36948a5baf4832430ce8834f018fa55
Evan Tan <lookuper@126.com>
2013-07-01 14:46:47 +0800

    Code base completed

---
+++ index.js
@@ -0,0 +1,112 @@
+/*jslint sub:true */
+var CMDS = ['  last n \t show last n log entries in database'],
+    LEVEL_INFO = 10,
+    LEVEL_DEBUG = 20,
+    LEVEL_WARN = 30,
+    LEVEL_ERROR = 40,
+    format = require('util').format;
+
+function find_plugin_config(config) {
+    for (var i = 0; i < config.plugins.length; i++) {
+        if (config.plugins[i].name === 'log') {
+            return config.plugins[i];
+        }
+    }
+    return null;
+}
+
+function showLast (n, db, collection) {
+    collection.find({}, {
+        'limit': n,
+        'sort': time
+    }).toArray(function(err, logs) {
+        var levelstr, i, data;
+        
+        if (err) {
+            console.error(err);
+            return db.close();
+        }
+        
+        for (i = 0; i < logs.length; i++) {
+            levelstr = 'info: ',
+            data = '-----> + ';
+        
+            if (logs[i].level === LEVEL_DEBUG) {
+                levelstr = 'debug: ';
+            } else if (logs[i].level === LEVEL_WARN) {
+                levelstr = 'warn: ';
+            } else if (logs[i].level === LEVEL_ERROR) {
+                levelstr = 'error: ';
+            }
+            data += levelstr;
+            data += format('%s %s \n', logs[i].time, logs[i].zone);
+            data += JSON.stringify(logs[i].msg) + '\n';
+            data += '<-----';
+            console.log(data);
+        }
+        db.close();
+    });
+}
+
+function docmd (argv, config, rootdir) {
+    var MongoClient = require('mongodb').MongoClient,
+        pconfig = {},
+        n;
+    if (argv.length === 0 || argv[0] === '-h' || argv[0] === '--help') {
+        return console.log('Available commands: \n' + CMDS[0] + '\n');
+    }
+    if (argv.length === 2 && argv[0] === 'last') {
+        n = Number(argv[1]);
+        if (n > 0 && n < 1000) {
+            pconfig = find_plugin_config(config);
+            if (!pconfig.db) {
+                return console.log('db-log is disabled, please use tail to see entries in file.');
+            }
+            MongoClient.connect(pconfig.db, function(err, db) {
+                if(err) throw err;
+                var collection = db.collection(pconfig.collection);
+                showLast(n, db, collection);
+            });
+        } else {
+            console.log('param n must between 0 and 1000');
+        }
+    }
+}
+
+function build (config, rootdir, next) {
+    var pconfig = find_plugin_config(config),
+        mkdirp = require('mkdirp'),
+        MongoClient = require('mongodb').MongoClient;
+    
+    try {
+        if (pconfig.file) {
+            mkdirp.mkdirp.sync(path.resolve(rootdir, pconfig.file, '..'));
+        }
+    } catch (err) {
+        console.error('Error when create log dir: ' + err);
+        return next('log');
+    }
+    if(pconfig.db) {
+        MongoClient.connect(pconfig.db, function(err, db) {
+            if (err) {
+                console.error('Cannot open db:' + pconfig.db);
+                db.close();
+                return next('log');
+            } else {
+                next();
+            }
+        });
+    }
+}
+
+function init (mm) {
+    var Log = require('./log'),
+        pconfig = find_plugin_config(mm.config);
+    new Log(mm, pconfig);
+}
+
+exports.docmd = docmd;
+exports.build = build;
+exports.post_build = null;
+exports.init = init;
+exports.post_init = null;
